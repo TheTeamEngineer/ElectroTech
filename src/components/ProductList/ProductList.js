bcf9d90564372aa9a046bdc0c788e22877e5d064
saquib khan <saquib.k765@gmail.com>
2022-01-31 02:16:12 +0530

    changed title

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -10,7 +10,7 @@ export default class ProductList extends Component {
   render() {
     return (
       <div className={classes.PageContainer}>
-        <Title name="Suitcase" />
+        <Title name="ElectroTech" />
         <div className ={classes.ProductContainer}>
           <ProductConsumer>
             {(value)=>{
