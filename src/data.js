399feb7a618d3838cec3ede6b72a791d72c412ae
Naqeeb Ahmed <Naqeeb532@outlook.com>
2022-01-30 18:52:40 +0530

    card deatil updated

--- src/data.js
+++ src/data.js
@@ -1,68 +1,68 @@
 export const storeProducts = [
   {
     id: 1,
-    title: "IT Luggage The LITE 4 Wheel Soft Cabin Suitcase",
-    img: "img/product-1.png",
-    price: 29.99,
-    company: "IT Luggage",
+    title: "Arduino Uno",
+    img: "img/arduino.png",
+    price: 500,
+    company: "Arduino",
     info:
-      "This super lightweight 4 Wheel LITE Cabin Case from IT Luggage is made with a soft nylon shell for minimal weight and extra durability. A specially developed platform is integrated into the base for strength and stability, capable of supporting 34 litres: combined with superior comfort carry handles and a fast access front panel pocket, it's sure to meet all your travel requirements. Designed in black with a pewter trim for smart neutrality, it's cabin-friendly.",
+      "The Arduino UNO is an AVR microcontroller. It is a board-based microcontroller (small computer on a chip) with facilities for processing data and I/O (input and output) pins for receiving and sending signals to devices. It can be powered and programmed using a computer or mobile phone. Arduino contains a mini/micro or type B USB port that can be used to connect to a PC via the regular USB port, and the connection is done via a USB cable. USBs Programmable Controller ATmega328 Chip You get Board and Cable set DC port available Arduino Shield Compatible",
     inCart: false,
     count: 0,
     total: 0
   },
   {
     id: 2,
-    title: "Go Explore Small 4 Wheel Hard Suitcase",
-    img: "img/product-2.png",
-    price: 16.99,
-    company: "Go Explore ",
+    title: "Raspberry pi 4",
+    img: "img/Raspberry_Pi_4.png",
+    price: 5000,
+    company: "Raspberry ",
     info:
-      "With a sizeable 49L capacity, the Go Explore 4 Wheel Case boasts a hard durable shell for impact protection while remaining lightweight at 2.8kg. 4 easy-glide, wide set corner wheels give 360 control and distribute weight evenly for a lighter feel and better wearing: the central handle has a push button release that is simple to expand and retract. In a sophisticated navy and tan design, you can easily spot your suitcase anywhere. Also comes in a medium and large size.",
+      "The Raspberry Pi 4 Model B is the newest Raspberry Pi computer made, and the Pi Foundation knows you can always make a good thing better! And what could make the Pi 4 better than the 3? How about a faster processor, USB 3.0 ports, and updated Gigabit Ethernet chip with PoE capability? Good guess - that's exactly what they did! The Raspberry Pi 4 is the latest product in the Raspberry Pi range, boasting an updated 64-bit quad core processor running at 1.5GHz with built-in metal heatsink, USB 3 ports, dual-band 2.4GHz and 5GHz wireless LAN, faster Gigabit Ethernet, and PoE capability via a separate PoE HAT. This version comes with 4 GB of RAM, but we also have versions with 1 and 2 GB if you like. You can still use all your favorite Raspbian or PIXEL software with this update. You MUST make sure to upgrade your Raspbian operating system install to the latest version so that the firmware can support the new chips! Old SD cards from previous releases will not work without a upgrade! The dual-band wireless LAN comes with modular compliance certification, that's the metal tin in the corner of the Pi, with the logo stamped on it. This allows the board to be designed into end products with significantly reduced wireless LAN compliance testing, improving both cost and time to market.",
     inCart: false,
     count: 0,
     total: 0
   },
   {
     id: 3,
-    title: "Ionian 8 Wheel Frameless Cabin Case",
-    img: "img/product-3.png",
-    price: 40,
-    company: "IT Luggage",
+    title: "KK Flight Controller ",
+    img: "img/KK-1.jpg",
+    price: 2500,
+    company: "KK",
     info:
-      "Stand out from the crowd with Ionian from IT Luggage - ideal for spotting on those busy carousels. This frameless case is typically 25 per cent lighter than conventional cases and no frame means more packing space. The constant design and testing ensures that this case has superior wheel control making it easy for you to glide through the crowds at those busy airports. The clamshell design helps seperate belonging, and provides an ideal flat packing surface. For extra reassurance this case comes with a 10 year warranty.",
+      "The next evolution of the rotor revolution is here. the kk-mini is packing all the same power, but with a mighty-mini form factor. The kk-mini is the next big/small evolution of the first generation kk flight control boards. The kk-mini is based on the kk2.1.5 architecture and has all the same inputs and outputs as well as the full functionality of its big brother. The 36mm designed was adapted to fit the needs of the mini market, but don't let its size fool you as it's just as much at home in any size multi-rotor. The kk mini still utilizes a built in gui using a hi-res mini lcd and 4 micro buttons for the same great kk experience. It was engineered from the ground up to bring multi-rotor flight to everyone, not just the experts. A host of multi-rotor craft types are pre-installed, simply select your craft type, check motor layout/propeller direction, calibrate your escs and radio and you're ready to go. all of which is done with easy to follow on screen prompts. The kk gyro system has been updated to an incredibly sensitive 6050 mpu system, making this the most stable kk board ever and allowing for the addition of an auto-level function. At the heart of the kk-mini is an atmel mega644pa 8-bit avr risc-based microcontroller with 64k of memory. An additional polarity protected header has been added for voltage detection, so no need for on-board soldering. A handy piezo buzzer is also included for audio warning when activating and deactivating the board.",
     inCart: false,
     count: 0,
     total: 0
   },
   {
     id: 4,
-    title: " Tourister Soundbox 8 Wheel Spinner 55",
-    img: "img/product-4.png",
-    price: 43,
-    company: "American Tourister",
+    title: "Multimeter",
+    img: "img/multimeter.png",
+    price: 200,
+    company: "",
     info:
-      "SoundBox is the first expandable American Tourister zipped PP case. A fixed 3 dial recessed TSA combination lock on all sizes gives maximum security. Made from resistant polypropylene. Elastic cross-ribbons in top and bottom compartment. The combination of the concentric wave design with the fine matt texture creates a playful design.",
+      "Small multimeter for your day to day needs. If you have basic knowledge about multimeters you can try this small multimeter for basic continuity checks and voltage measurements. Functions: DCV, ACV, DCA. Resistor (ohms). Diode. Buzzer. Transistor (HFE). Temperature. Square wave output. Battery test. Display: 4.75 centimeter length, 15 millimeter high LCD. Operating Instructions: When testing DC voltage, AC voltage, DC current, resistance, diode, buzzer and battery, connect the red test lead to V 0 mA jack and black test lead to COM jack. When testing the current more than 200mA, connect the red test lead to OADC jack and black test lead to number 2022;COM jack. When testing the temperature, connect the temperature probe to V, 0, mA jack or COM jack. It can also be connected to the jack specialized for it. When testing the magnifying ratio of the transistor, turn the switch to hFE position and connect the transistor to the eight-hole jack. Square Wave Output: The frequency is 50Hz, the voltage is approximate. 3Vp-p. The output jack is V, 0, mA and number 2022. Powered by a 9V battery inside. Dimensions of Multimeter: 12.5 centimeters (Length) x 6.75 centimeters (Width) x 2.25 centimeters (Thickness). Wire Length (Including Pin): 71 centimeters each.",
     inCart: false,
     count: 0,
     total: 0
   },
   {
     id: 5,
-    title: "Bon Air 4 Wheel Spinner",
-    img: "img/product-5.png",
+    title: "NodeMcu",
+    img: "img/nodemcu.png",
     price: 50,
     company: "American Tourister",
     info:
-      "Lightweight. Secure: 3 dial TSA lock on all sizes keeping your belongings secure. Organised and storage: cross-ribbons in matching outside colour in bottom compartment. It includes a mesh divider with additional zipped mesh pocket and apron pocket, offering extra space to storage your belongings.",
+      "Esp8266 is a highly integrated chip designed for the needs of a new connected world. It offers a complete and self-contained wi-fi networking solution, allowing it to either host the application or to offload all wi-fi networking functions from another application processor.",
     inCart: false,
     count: 0,
     total: 0
   },
   {
     id: 6,
-    title: "World's Lightest 2 Wheel Cabin Suitcase",
-    img: "img/product-6.png",
+    title: "Servo Motor",
+    img: "img/servo.png",
     price: 60,
     company: "IT ",
     info:
@@ -72,28 +72,28 @@ export const storeProducts = [
     total: 0
   },
   {
-    id: 7,
-    title: "Small Expandable 4 Wheel Hard Suitcase",
-    img: "img/product-7.png",
-    price: 40,
-    company: "apple",
-    info:
-      "This 4 Wheel Large Suitcase from IT Luggage features a hard ABS shell for secure impact protection. A single expander system accounts for an extra 25 per cent packing capacity so that it can fit 42L- the hard shell expander feature is unique to IT Luggage, so you won't find it anywhere else! Ideal for business trips as well as holidays and weekend breaks, this small cabin case is hardwearing and versatile: green details on the handle and the suitcase itself mean you'll never lose it on the luggage carousel.",
-    inCart: false,
-    count: 0,
-    total: 0
+    // id: 7,
+    // title: "Small Expandable 4 Wheel Hard Suitcase",
+    // img: "img/product-7.png",
+    // price: 40,
+    // company: "apple",
+    // info:
+    //   "This 4 Wheel Large Suitcase from IT Luggage features a hard ABS shell for secure impact protection. A single expander system accounts for an extra 25 per cent packing capacity so that it can fit 42L- the hard shell expander feature is unique to IT Luggage, so you won't find it anywhere else! Ideal for business trips as well as holidays and weekend breaks, this small cabin case is hardwearing and versatile: green details on the handle and the suitcase itself mean you'll never lose it on the luggage carousel.",
+    // inCart: false,
+    // count: 0,
+    // total: 0
   },
   {
-    id: 8,
-    title: "Asteroid 2 Wheel Hard Cabin Suitcase",
-    img: "img/product-8.png",
-    price: 25,
-    company: "apple",
-    info:
-      "The virtually unbreakable polypropylene shell featured on Asteroid by it Luggage offers the best protection for your belongings. The cabin friendly size makes this case a great companion on any trip to ensure you have all the things you need for the flight at easy reach. The central opening is designed to provide an ideal flat packing surface whilst the expander system gives 25% extra space when you need it most without compromising on the suitcases stability, making this the ideal case for business trips as well as holidays and weekends away.",
-    inCart: false,
-    count: 0,
-    total: 0
+    // id: 8,
+    // title: "Asteroid 2 Wheel Hard Cabin Suitcase",
+    // img: "img/product-8.png",
+    // price: 25,
+    // company: "apple",
+    // info:
+    //   "The virtually unbreakable polypropylene shell featured on Asteroid by it Luggage offers the best protection for your belongings. The cabin friendly size makes this case a great companion on any trip to ensure you have all the things you need for the flight at easy reach. The central opening is designed to provide an ideal flat packing surface whilst the expander system gives 25% extra space when you need it most without compromising on the suitcases stability, making this the ideal case for business trips as well as holidays and weekends away.",
+    // inCart: false,
+    // count: 0,
+    // total: 0
   }
 ];
 
