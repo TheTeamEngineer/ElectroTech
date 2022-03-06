34454ae12dde53b41547fb95443392cc9482e9e2
Arnab Kumar <arnab.d.kumar@gmail.com>
2022-03-06 20:58:57 +0530

    Cover Page updated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,35 +1,39 @@
 import React, { Component } from "react";
 import classes from "./Home.css";
-import Robot from "./../../assets/img/robot.jpg";
+import Robot from "./../../assets/img/hero_shape.png";
+import Robo from "./../../assets/img/robo.png";
 import { NavLink } from "react-router-dom";
-import Footer from '../Footer/Footer'
-import Faq from '../FAQ/Faq'
-  
+import Footer from "../Footer/Footer";
+import Faq from "../FAQ/Faq";
+import { FaChevronRight } from "react-icons/fa";
+
 
 export default class Home extends Component {
   render() {
     return (
-      <div> 
-      <div className={classes.Container}>
-        <img src={Robot} alt="home" className={classes.Img} />
-        <div className={classes.Overlay} />
-        <div className={classes.Headding}>
-          <h1 className={classes.Title}>
-            Thoughtful standards for modern Robotics
-          </h1>
-          <NavLink to ="/shop">
-            <button className={classes.Btn}>shop now</button>
-          </NavLink>
+      <div>
+        <div className={classes.Container}>
+          <img src={Robot} alt='home' className={classes.Img} />
+          <div className={classes.heroSection}>
+            <div className={classes.Headding}>
+              <h1 className={classes.Title}>
+                <span className={classes.underline}>Thoughtful</span>
+                <br /> standards for <br /> modern Robotics
+              </h1>
+              <div>
+                <NavLink className={classes.Btn} to='/shop'>
+                  <span>Shop Now</span>
+                  <FaChevronRight className={classes.BtnIcon} />
+                </NavLink>
+              </div>
+            </div>
+            <img src={Robo} alt='home' className={classes.roboImg} />
+          </div>
         </div>
- 
-       </div>
-       <Faq/>
+        <Faq />
 
-		<Footer />
+        <Footer />
       </div>
-      
-       
-            
     );
   }
 }
