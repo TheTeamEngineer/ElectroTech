392295866ec09cb666b3ebba88753efc597a99a0
Anushka Chauhan <anushkachauhan1503@gmail.com>
2022-03-13 19:51:23 +0530

    Chat Button Changes

--- src/components/Chatbot/Chatbot.js
+++ src/components/Chatbot/Chatbot.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState,useRef } from 'react'
+import { useState, useRef } from 'react'
 import classes from "./Chatbot.css";
 import chat from './Chat.css';
 import logo from "./../../assets/img/chatbot.png";
@@ -11,19 +11,17 @@ const Chatbot = () => {
   //  Make ID function, I use this to create random ID
   // credit : https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
   const makeid = (length) => {
-  var result = '';
-  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  var charactersLength = characters.length;
-  for ( var i = 0; i < length; i++ ) {
-  result += characters.charAt(Math.floor(Math.random() * 
-  charactersLength));
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() *
+        charactersLength));
+    }
+    return result;
   }
-  return result;
-  }
-
 
   const ChatInput = useRef(null);
-
 
   const [ChatOpen, setChatOpen] = useState(false)
 
@@ -33,7 +31,6 @@ const Chatbot = () => {
       message: "Hello, how can I help you?",
       sender: "Bot"
     },
-   
   ]
 
   const [sessionID, setSessionID] = useState(makeid(10))
@@ -50,100 +47,94 @@ const Chatbot = () => {
     ReciveMsg(UserMessage);
     setUserMessage("")
   }
-  
-  const ReciveMsg = async (msg) =>{
-      
-      const message = {
-        id: Messages.length + 1,
-        message: msg,
-        sender: "User"
-      }
-      
-      setMessages([...Messages, message]);
-      
-      await fetch(`https://electro-tech-bot-api.herokuapp.com/chat-bot-api?msg=${msg}&id=${sessionID}`)
+
+  const ReciveMsg = async (msg) => {
+    const message = {
+      id: Messages.length + 1,
+      message: msg,
+      sender: "User"
+    }
+
+    setMessages([...Messages, message]);
+
+    await fetch(`https://electro-tech-bot-api.herokuapp.com/chat-bot-api?msg=${msg}&id=${sessionID}`)
       .then(response => response.json())
       .then(data => {
         const botMsg = { id: Messages.length + 1, message: data.message, sender: "Bot" }
-        setMessages([ ...Messages, message, botMsg ]);
-          setReceivingMsg(false)
-        });
-  
+        setMessages([...Messages, message, botMsg]);
+        setReceivingMsg(false)
+      });
+  }
+
+  const onEnterPress = (e) => {
+    if (e.keyCode === 13 && e.shiftKey === false) {
+      e.preventDefault();
+      ReciveMsg(UserMessage);
+      setUserMessage("")
     }
-    
-    const onEnterPress = (e) => {
-      if(e.keyCode === 13 && e.shiftKey === false) {
-        e.preventDefault();
-        ReciveMsg(UserMessage);
-        setUserMessage("")
-      }
-    }
-    
+  }
+
   return (
     <div className={classes.container}>
+      {/* Chat Area */}
       <div className={classes.Chatbot}>
+        {ChatOpen &&
+          <div className={chat.chat}>
+            <div className={chat.chatTitle}>
+              <h1>Electro Tech</h1>
+              <h2>Chatbot</h2>
+              <figure className={chat.avatar}>
+                <img src={logo} alt='' />
+              </figure>
+            </div>
 
-        {  ChatOpen &&
+            <div className={chat.messages}>
+              <div className={chat.messagesContent}>
+                {Messages.map(message => {
+                  if (message.sender === "User") {
+                    return (
+                      <div key={message.id} className={`${chat.message} ${chat.messagePersonal}`}>
+                        {message.message}
+                      </div>
+                    )
+                  } else {
+                    return (
+                      <div key={message.id} className={`${chat.new} ${chat.message}`}>
+                        <figure className={chat.avatar}>
+                          <img src={logo} alt='' />
+                        </figure>
+                        {message.message}
+                      </div>
+                    )
+                  }
+                })}
 
-        <div className={chat.chat}>
-          <div className={chat.chatTitle}>
-            <h1>Electro Tech</h1>
-            <h2>Chatbot</h2>
-            <figure className={chat.avatar}>
-              <img src={logo} alt=''/>
-            </figure>
-          </div>
+                {/* Loading Messages */}
+                {ReceivingMsg &&
+                  <div className={`${chat.new} ${chat.message} ${chat.loading}`}>
+                    <figure className={chat.avatar}>
+                      <img src={logo} alt='' />
+                    </figure>
+                    <span></span>
+                  </div>
+                }
+              </div>
+            </div>
 
-          <div className={chat.messages}>
-            <div className={chat.messagesContent}>
-
-              {Messages.map(message => {
-                if (message.sender === "User") {
-                  return (
-                    <div key={message.id} className={`${chat.message} ${chat.messagePersonal}`}>
-                      {message.message}
-                    </div>
-                  )
-                }else {
-                  return (
-                    <div key={message.id} className={`${chat.new} ${chat.message}`}>
-                      <figure className={chat.avatar}>
-                        <img src={logo} alt='' />
-                      </figure>
-                      {message.message}
-                    </div>
-                  )}
-              })}
-              
-              {/* Loading Meassages */}
-              {
-                ReceivingMsg &&
-                <div className={`${chat.new} ${chat.message} ${chat.loading}`}>
-                  <figure className={chat.avatar}>
-                    <img src={logo} alt='' />
-                  </figure>
-                  <span></span>
-                </div>
-              }
-
+            {/* Text Area */}
+            <div className={chat.messageBox}>
+              <form onSubmit={(e) => { handelSubmit(e) }} ref={ChatInput}>
+                <textarea type="text" name='message' value={UserMessage} onChange={(e) => { setUserMessage(e.target.value) }} className={chat.messageInput} onKeyDown={(e) => { onEnterPress(e) }} placeholder="Type message..."></textarea>
+                <button type="submit" className={chat.messageSubmit}>Send</button>
+              </form>
             </div>
           </div>
-
-          {/* Text Area */}
-          <div className={chat.messageBox}>
-            <form onSubmit={(e)=>{handelSubmit(e)}} ref={ChatInput}>
-              <textarea type="text" name='message' value={UserMessage} onChange={(e)=>{setUserMessage(e.target.value)}} className={chat.messageInput} onKeyDown={(e)=>{onEnterPress(e)}} placeholder="Type message..."></textarea>
-              <button type="submit"  className={chat.messageSubmit}>Send</button>
-            </form>
-          </div>
-
-        </div>
-        
         }
-      
       </div>
+
+      {/* Chat CTA Button */}
       <div className={classes.CallToAction} onClick={() => setChatOpen(!ChatOpen)}>
-        { ChatOpen ? <FaTimes size={30}/> : <BsChatFill size={30} /> }
+        {ChatOpen ? <FaTimes size={30} /> : <BsChatFill size={28} />}
       </div>
     </div>
   )
